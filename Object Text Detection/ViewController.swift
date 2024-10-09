0ff253901031b773ca644b76d72418fe2c54dc63
Raja <raja@tothenew.com>
2024-10-09 14:32:13 +0530

    Initial Commit

---
+++ Object Text Detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Object Text Detection
+//
+//  Created by Raja on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
